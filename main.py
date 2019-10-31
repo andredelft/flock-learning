f0ca5cb268dcc5f8a2caf428fd5ae3f1c3d64ea8
André van Delft <andrevandelft@outlook.com>
2019-10-31 14:25:37 +0100

    Vicsek vs instinct

--- main.py
+++ main.py
@@ -5,8 +5,8 @@ import matplotlib.animation as animation
 import numpy as np
 from scipy.spatial import KDTree
 
-r_0 = 0.5
-R = 10
+r_0 = 0.3   # Stepsize
+R = 10      # Sight radius (length )
 N = 4
 
 class Flock(object):
@@ -14,9 +14,10 @@ class Flock(object):
     def __init__(self, numpoints=50, fname=None):
         self.numpoints = numpoints
         self.birds = (np.random.random((self.numpoints, 2))-0.5)*200
-        self.I = 2 * np.pi * np.random.random(self.numpoints) # instinct angle
-        self.dirs = self.I
+        self.instincts = 2 * np.pi * np.random.random(self.numpoints) # instinct angle
+        self.dirs = self.instincts # Initial flight angle
         self.stream = self.data_stream()
+        self.trusts = np.random.random(self.numpoints)
 
         # Setup the figure and axes...
         self.fig, self.ax = plt.subplots()
@@ -36,34 +37,36 @@ class Flock(object):
     def setup_plot(self):
         """Initial drawing of the scatter plot."""
         x, y, = next(self.stream).T
-        self.scat = self.ax.scatter(x, y, vmin=0, vmax=1, edgecolor="k")
+        self.scat = self.ax.scatter(x, y, vmin=0, vmax=1, c=self.trusts,
+                                    cmap="coolwarm", edgecolor="k")
         self.ax.axis([-100, 100, -100, 100])
         # For FuncAnimation's sake, we need to return the artist we'll be using
         # Note that it expects a sequence of artists, thus the trailing comma.
         return self.scat,
 
-    def vicsek(self, no_neigbours=N, max_radius=R):
+    def partial_vicsek(self, no_neigbours=N, max_radius=R):
         tree = KDTree(self.birds)
         new_dirs = []
         for i,bird in enumerate(self.birds):
-            neighbours = sorted(
-                tree.query_ball_point(bird, max_radius),
-                key = lambda j: np.linalg.norm(bird - self.birds[j])
-            )[:N+1] # N nearest neigbours and itself
-            new_dirs.append(
-                # Circular mean of nearest neigbours (itself included)
-                np.arctan2(
-                    sum(np.sin(self.dirs[n]) for n in neighbours)/len(neighbours),
-                    sum(np.cos(self.dirs[n]) for n in neighbours)/len(neighbours)
+            if np.random.random() < self.trusts[i]:
+                neighbours = sorted(
+                    tree.query_ball_point(bird, max_radius),
+                    key = lambda j: np.linalg.norm(bird - self.birds[j])
+                )[:N+1] # N nearest neigbours and itself
+                new_dirs.append(
+                    # Circular mean of nearest neigbours (itself included)
+                    np.arctan2(
+                        sum(np.sin(self.dirs[n]) for n in neighbours)/len(neighbours),
+                        sum(np.cos(self.dirs[n]) for n in neighbours)/len(neighbours)
+                    )
                 )
-            )
+            else:
+                new_dirs.append(self.instincts[i])
         self.dirs = new_dirs
 
     def data_stream(self):
-        """Generate a random walk (brownian motion). Data is scaled to produce
-        a soft "flickering" effect."""
         while True:
-            self.vicsek()
+            self.partial_vicsek()
             self.birds += [[r_0 * np.cos(theta), r_0 * np.sin(theta)] for theta in self.dirs]
             # Periodic boundaries
             for i in range(self.birds.shape[0]):
@@ -87,5 +90,5 @@ class Flock(object):
 
 
 if __name__ == '__main__':
-    a = Flock(200, fname="vicsek.mp4")
-    # plt.show()
+    a = Flock(50)#, fname="vicsek.mp4")
+    plt.show()
