d02b65cdf9d55720a867a3113cbe734af463c757
André van Delft <andrevandelft@outlook.com>
2019-10-31 17:48:14 +0100

    Added some recorded simulations

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 import numpy as np
 from scipy.spatial import KDTree
+from os import path
 
 r_0 = 0.3   # Stepsize
 R = 10      # Sight radius (length )
@@ -24,11 +25,12 @@ class Flock(object):
         # Then setup FuncAnimation.
         if fname:
             writer = animation.FFMpegWriter(fps=15)
-            with writer.saving(self.fig, fname, 100):
+            with writer.saving(self.fig, path.join('movies',fname), 100):
                 self.setup_plot()
-                for i in range(100):
+                for i in range(400):
                     writer.grab_frame()
-                    self.update(i)
+                    for _ in range(5):
+                        self.update()
         else:
             self.ani = animation.FuncAnimation(
                 self.fig, self.update, interval=5,init_func=self.setup_plot, blit=True
@@ -77,7 +79,7 @@ class Flock(object):
                         self.birds[i,j] += 200
             yield np.c_[self.birds[:,0], self.birds[:,1]]
 
-    def update(self, i):
+    def update(self):
         """Update the scatter plot."""
         data = next(self.stream)
 
@@ -90,5 +92,5 @@ class Flock(object):
 
 
 if __name__ == '__main__':
-    a = Flock(50)#, fname="vicsek.mp4")
+    a = Flock(50, fname="vicsek-instinct.mp4")
     plt.show()
