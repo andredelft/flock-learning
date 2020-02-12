0449918b0991dedaa924d713cd5cc0441ef23247
André van Delft <andrevandelft@outlook.com>
2020-02-12 12:54:58 +0100

    Function for averaging out noise.

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 from field import Field
 from matplotlib import pyplot as plt
 
+def avg(data, cap = 20):
+    return [sum(data[i:i + cap])/cap for i in range(len(data) - cap)]
+
 if __name__ == '__main__':
     Field(100, periodic = True, record = True)
     plt.show()
