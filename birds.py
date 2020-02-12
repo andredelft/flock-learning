1ed045137baacd99b1a049ee05fdecbd1af247ef
André van Delft <andrevandelft@outlook.com>
2020-02-12 11:30:55 +0100

    Add option of observing flight direction of neighbours

--- birds.py
+++ birds.py
@@ -2,9 +2,16 @@ import numpy as np
 from random import randint, choice, choices
 from scipy.spatial import KDTree
 
-R = 100
+R = 100 # Observation radius
 
 A = ['N','E','S','W','I'] # Action space
+
+step = {
+    'N': np.array([ 0, 1]),
+    'E': np.array([ 1, 0]),
+    'S': np.array([ 0,-1]),
+    'W': np.array([-1, 0])
+}
 
 def circular_mean(dirs):
     return np.arctan2(sum(np.sin(dirs))/len(dirs),sum(np.cos(dirs))/len(dirs))
@@ -21,16 +28,17 @@ class Birds(object):
     def __init__(self, numbirds, field_dims):
         self.numbirds = numbirds
 
-        # Initialization
+        # Initialization of birds
         self.positions = np.array([
             np.array([
                 randint(*field_dims[0:2]), randint(*field_dims[2:4])
             ]) for _ in range(self.numbirds)
         ])
+        self.dirs = [choice(A) for _ in range(self.numbirds)]
         self.instincts = [choice(['N','E','S','W']) for _ in range(self.numbirds)]
         self.policies = 100/5 + np.zeros([self.numbirds,3**4,5])
 
-    def observe(self, bird_index, radius=R):
+    def observe(self, bird_index, radius = R, direction_hist = False):
         tree = KDTree(self.positions)
         neighbours_inds = tree.query_ball_point(self.positions[bird_index], radius)
         neighbours_inds.remove(bird_index)
@@ -40,21 +48,38 @@ class Birds(object):
             'S': 0,
             'W': 0
         }
-        for i in neighbours_inds:
-            delta_x = self.positions[bird_index,0] - self.positions[i,0]
-            delta_y = self.positions[bird_index,1] - self.positions[i,1]
-            angle = np.arctan2(delta_y,delta_x)
-            if (angle < -3 * np.pi / 4) or (angle >= 3 * np.pi / 4):
-                neighbours['E'] += 1
-            elif (angle < 3 * np.pi / 4) and (angle >= np.pi / 4):
-                neighbours['N'] += 1
-            elif (angle < np.pi / 4) and (angle >= -1 * np.pi / 4):
-                neighbours['W'] += 1
-            elif (angle < -1 * np.pi/4) and (angle >= -3 * np.pi / 4):
-                neighbours['S'] += 1
-            else:
-                # Check if all cases are catched
-                raise ValueError(f'No value found for angle {angle}')
+
+        # Two possible observations:
+        # – Relative position of neighbours (direction_hist = False): Divides
+        #   the observation space in four quadrants (N, E, S, W) and counts the
+        #   number of birds in each quandrant.
+        # - Flight direction of neighbours (direction_hist = True): Observes
+        #   and counts the current flight direction of all birds in the
+        #   observation space.
+        if not direction_hist:
+            for i in neighbours_inds:
+                delta_x = self.positions[bird_index,0] - self.positions[i,0]
+                delta_y = self.positions[bird_index,1] - self.positions[i,1]
+                angle = np.arctan2(delta_y,delta_x)
+                if (angle < -3 * np.pi / 4) or (angle >= 3 * np.pi / 4):
+                    neighbours['E'] += 1
+                elif (angle < 3 * np.pi / 4) and (angle >= np.pi / 4):
+                    neighbours['N'] += 1
+                elif (angle < np.pi / 4) and (angle >= -1 * np.pi / 4):
+                    neighbours['W'] += 1
+                elif (angle < -1 * np.pi/4) and (angle >= -3 * np.pi / 4):
+                    neighbours['S'] += 1
+                else:
+                    # Check if all cases are catched
+                    raise ValueError(f'No value found for angle {angle}')
+        else:
+            for i in neighbours_inds:
+                if self.dirs[i] == 'I':
+                    dir = self.instincts[i]
+                else:
+                    dir = self.dirs[i]
+                neighbours[dir] += 1
+
         # Maximum of 2
         for dir in neighbours.keys():
             if neighbours[dir] > 2:
