a436cddeacbbf8aa93b03efd31447c3b062d8a7c
André van Delft <andrevandelft@outlook.com>
2020-02-19 19:39:33 +0100

    New data

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from field import Field
 from matplotlib import pyplot as plt
 import numpy as np
+from os import path
 
 def avg(data, cap = 20):
     return [sum(data[i:i + cap])/cap for i in range(len(data) - cap)]
@@ -16,6 +17,16 @@ def plot_vx(fname, label = ''):
 def plot_arg(fname, label = ''):
     data = np.load(fname)
     plt.plot(avg([np.arctan2(v[1],v[0]) for v in data], cap=100),label=label)
+
+def plot_mag_arg(fname):
+    data = np.load(fname)
+    fig,a = plt.subplots(2,1)
+    a[0].set_title(path.split(fname)[1])
+    a[0].plot(avg([np.linalg.norm(v) for v in data], cap=50))
+    a[0].set_ylabel('$|\mathbf{v}|$')
+    a[1].set_xlabel('Timestep')
+    a[1].set_ylabel('Arg(v)')
+    a[1].plot(avg([np.arctan2(v[1],v[0]) for v in data], cap=100))
 
 if __name__ == '__main__':
     Field(100, periodic = True, plot = False, record_data = True, observe_direction = True)
