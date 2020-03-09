0fb06414b206f972977204520d87feda4c46abd9
André van Delft <andrevandelft@outlook.com>
2020-03-09 17:19:59 +0100

    More readable update function

--- q_learning.py
+++ q_learning.py
@@ -1,11 +1,12 @@
 from numpy.random import rand
 
-# Sample state and action space
+""" Sample state and action space:
 A = ['N','E','S','W']
-S = [0,1,2,3,4]
+S = [0,1,2,3,4] # Assume S is a list of its indices (e.g. S == list(range(len(S))))
+"""
 
 class Qfunction(object):
-    def __init__(self, alpha, beta, A = A, S = S):
+    def __init__(self, alpha, beta, A, S):
         if alpha < 0 or alpha >= 1:
             raise ValueError("0 <= alpha < 1 is required")
         self.alpha = alpha
@@ -17,5 +18,6 @@ class Qfunction(object):
         self.value = rand(len(self.S),len(self.A))
 
     def update(self, s, a, s_prime, r):
-        self.value[self.S.index(s), self.A.index(a)] = (1 - self.alpha) * self.value[self.S.index(s), self.A.index(a)] \
-                                             + self.alpha * (r + self.beta * max(self.value[self.S.index(s_prime), self.A.index(a_prime)] for a_prime in A))
+        a_ind = self.A.index(a)
+        max_Q = max(self.value[s_prime, self.A.index(a_prime)] for a_prime in A)
+        self.value[s, self.A.index(a)] = (1 - self.alpha) * self.value[s, a_ind] + self.alpha * (r + self.beta * max_Q)
