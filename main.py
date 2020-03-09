001d9f37672f739a3e82b8bbbbd3dfea4211d09a
André van Delft <andrevandelft@outlook.com>
2020-03-09 17:11:06 +0100

    Today's data

--- main.py
+++ main.py
@@ -2,6 +2,6 @@ from field import Field
 
 if __name__ == '__main__':
     Field(
-        100, periodic = True, plot = True, record_data = False, observe_direction = True,
-        track_birds = False, sim_length = 15000, learning_alg = 'Ried'
+        100, periodic = True, plot = True, record_data = True, observe_direction = True,
+        track_birds = True, sim_length = 15000, learning_alg = 'Q'
     )
