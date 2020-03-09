2c577e2a8e5bf014e56331a4124f06dabdaa1aea
André van Delft <andrevandelft@outlook.com>
2020-03-09 14:45:00 +0100

    First Q-learning snippet

---
+++ q_learning.py
@@ -0,0 +1,21 @@
+from numpy.random import rand
+
+# Sample state and action space
+A = ['N','E','S','W']
+S = [0,1,2,3,4]
+
+class Qfunction(object):
+    def __init__(self, alpha, beta, A = A, S = S):
+        if alpha < 0 or alpha >= 1:
+            raise ValueError("0 <= alpha < 1 is required")
+        self.alpha = alpha
+        if beta < 0 or beta >= 1:
+            raise ValueError("0 <= beta < 1 is required")
+        self.beta = beta
+        self.A = A
+        self.S = S
+        self.value = rand(len(self.S),len(self.A))
+
+    def update(self, s, a, s_prime, r):
+        self.value[S.index(s), A.index(a)] = (1 - self.alpha) * self.value[S.index(s), A.index(a)] \
+                                             + self.alpha * (r + self.beta * max(self.value[S.index(s_prime), A.index(a_prime)] for a_prime in A))
