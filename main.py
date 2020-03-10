1079146eda9510078539e0b5c78cbeeded5458a8
André van Delft <andrevandelft@outlook.com>
2020-03-10 19:56:36 +0100

    Test data for varying alpha & gamma

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
 from field import Field
 
 if __name__ == '__main__':
-    Field(
-        100, periodic = True, plot = True, record_data = True, observe_direction = True,
-        track_birds = True, sim_length = 15000, learning_alg = 'Q'
-    )
+    for alpha in [0.9,0.7,0.5,0.3,0.1]:
+        Field(
+            100, periodic = True, plot = False, record_data = True, observe_direction = True,
+            track_birds = False, sim_length = 5000, learning_alg = 'Q', comment = 'vary_alpha',
+            alpha = alpha, gamma = 0.9
+        )
+    for gamma in [0.9,0.7,0.5,0.3,0.1]:
+        Field(
+            100, periodic = True, plot = False, record_data = True, observe_direction = True,
+            track_birds = False, sim_length = 5000, learning_alg = 'Q', comment = 'vary_gamma',
+            alpha = 0.9, gamma = gamma
+        )
