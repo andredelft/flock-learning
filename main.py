74f00a9bdf4d1624eae5cdb8c70ab7b50255b2da
André van Delft <andrevandelft@outlook.com>
2020-03-23 12:37:27 +0100

    20200319 data

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from field import Field
 
 if __name__ == '__main__':
-    Field(
-        100, periodic = True, plot = False, record_data = True, observe_direction = True,
-        sim_length = 5000, learning_alg = 'Q', alpha = 0.9, gamma = 0.9
-    )
+    for r in [1,2,5,10]:
+        Field(
+            100, periodic = True, plot = True, record_data = True, observe_direction = True,
+            sim_length = 12500, learning_alg = 'Ried', comment = 'vary:reward_signal',
+            reward_signal = r#, alpha = 0.9, gamma = 0.9
+        )
