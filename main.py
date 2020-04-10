5b528a42e67f4bd4e5401343e60cea1744fc7327
André van Delft <andrevandelft@outlook.com>
2020-04-10 12:56:38 +0200

    Try out the trained Q-values of yesterday

--- main.py
+++ main.py
@@ -18,5 +18,5 @@ def load_from_Q(record_tag, data_dir = 'data', plot = True, record_data = True, 
     )
 
 if __name__ == '__main__':
-    # load_from_Q('20200408-154807', record_data = True, record_mov = True, sim_length = 5000)
-    Field(100, record_data = True, observe_direction = True, plot = True, learning_alg = 'Q')
+    load_from_Q('20200409-164937', data_dir = 'data/20200409', record_data = True, record_mov = True, sim_length = 10000)
+    # Field(100, record_data = True, observe_direction = True, plot = True, learning_alg = 'Q')
